5cdc70251405a20e60397ff40994b405d5963588
Jen Merritt <jenjomez@gmail.com>
2019-12-06 20:20:36 +0000

    read and update screenings

--- models/screening.rb
+++ models/screening.rb
@@ -10,8 +10,8 @@ class Screening
   def initialize( options )
     @id = options['id'].to_i if options['id']
     @showing_time = options['showing_time']
-    @capacity = options['capacity'].to_int
-    @film_id = options['film_id'].to_int
+    @capacity = options['capacity'].to_i
+    @film_id = options['film_id'].to_i
   end
 
 # CREATE / SAVE
@@ -33,7 +33,29 @@ class Screening
 
 # READ
 
+  def self.all()
+    sql = "SELECT * FROM screenings;"
+    screenings = SqlRunner.run(sql)
+    return screenings.map{ |screening| Screening.new(screening)}
+  end
+
 # UPDATE
+
+  def update()
+    sql = "UPDATE screenings
+    SET
+    (
+      showing_time,
+      capacity,
+      film_id
+    ) =
+    (
+      $1, $2, $3
+    )
+    WHERE id = $4"
+    values = [@showing_time, @capacity, @film_id, @id]
+    SqlRunner.run(sql, values)
+  end
 
 # DELETE
 
