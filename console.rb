28f147e4fc83e48bfd5d7c65d10d39398db877ea
Jen Merritt <jenjomez@gmail.com>
2019-12-06 20:17:19 +0000

    setup class for screenings and save/delete methods

--- console.rb
+++ console.rb
@@ -5,8 +5,11 @@ require_relative( 'models/film' )
 require('pry')
 
 Ticket.delete_all
+Screening.delete_all
 Film.delete_all
 Customer.delete_all
+
+#### CUSTOMERS:
 
 customer1 = Customer.new({
   'name' => 'Regina Phalange',
@@ -20,17 +23,37 @@ customer2 = Customer.new({
   })
 customer2.save()
 
+customer3 = Customer.new({
+  'name' => 'Chanandler Bong',
+  'funds' => 90
+  })
+customer3.save()
+
+customer4 = Customer.new({
+  'name' => 'Ross Gellar',
+  'funds' => 50
+  })
+customer4.save()
+
+customer5 = Customer.new({
+  'name' => 'Monica Gellar',
+  'funds' => 70
+  })
+customer5.save()
+
+customer6 = Customer.new({
+  'name' => 'Rachel Green',
+  'funds' => 15
+  })
+customer6.save()
+
+#### FILMS:
+
 film1 = Film.new({
   'title' => 'Frozen 2',
   'price' => 8
   })
 film1.save()
-
-ticket1 = Ticket.new({
-  'customer_id' => customer1.id,
-  'film_id' => film1.id
-  })
-ticket1.save()
 
 film2 = Film.new({
   'title' => 'Last Christmas',
@@ -38,6 +61,73 @@ film2 = Film.new({
   })
 
 film2.save()
+
+film3 = Film.new({
+  'title' => 'Little Women',
+  'price' => 15
+  })
+
+film3.save()
+
+
+#### SCREENINGS:
+
+screening1 = Screening.new({
+  'showing_time' => "12:00",
+  'capacity' => 5,
+  'film_id' => film1.id
+  })
+
+screening1.save()
+
+screening2 = Screening.new({
+  'showing_time' => "14:00",
+  'capacity' => 7,
+  'film_id' => film2.id
+  })
+
+screening2.save()
+
+screening3 = Screening.new({
+  'showing_time' => "16:00",
+  'capacity' => 10,
+  'film_id' => film3.id
+  })
+
+screening3.save()
+
+screening4 = Screening.new({
+  'showing_time' => "18:00",
+  'capacity' => 20,
+  'film_id' => film1.id
+  })
+
+screening4.save()
+
+screening5 = Screening.new({
+  'showing_time' => "19:00",
+  'capacity' => 120,
+  'film_id' => film2.id
+  })
+
+screening5.save()
+
+screening6 = Screening.new({
+  'showing_time' => "20:00",
+  'capacity' => 150,
+  'film_id' => film3.id
+  })
+
+screening6.save()
+
+
+#### TICKETS:
+
+ticket1 = Ticket.new({
+  'customer_id' => customer1.id,
+  'film_id' => film1.id
+  })
+ticket1.save()
 
 ticket2 = Ticket.new({
   'customer_id' => customer2.id,
--- models/screening.rb
+++ models/screening.rb
@@ -4,4 +4,44 @@ require_relative('film')
 
 class Screening
 
+  attr_reader :id
+  attr_accessor :showing_time, :capacity, :film_id
+
+  def initialize( options )
+    @id = options['id'].to_i if options['id']
+    @showing_time = options['showing_time']
+    @capacity = options['capacity'].to_int
+    @film_id = options['film_id'].to_int
+  end
+
+# CREATE / SAVE
+
+  def save()
+    sql = "INSERT INTO screenings
+    (
+      showing_time,
+      capacity,
+      film_id
+    )
+    VALUES(
+      $1, $2, $3)
+      RETURNING id;"
+    values = [@showing_time, @capacity, @film_id]
+    ticket = SqlRunner.run(sql, values)[0]
+    @id = ticket['id'].to_i
+  end
+
+# READ
+
+# UPDATE
+
+# DELETE
+
+  def self.delete_all()
+    sql = "DELETE FROM screenings;"
+    SqlRunner.run(sql)
+  end
+
+
+
 end
