e9e8ea0fbc373a3b583bd22c5c47340aefbebb1e
Jen Merritt <jenjomez@gmail.com>
2019-12-06 18:36:04 +0000

    find number of customers seeing a film

--- models/film.rb
+++ models/film.rb
@@ -47,6 +47,12 @@ class Film
     return array_of_customers
   end
 
+# number of customers:
+  def number_of_customers()
+    list_of_customers = self.customers
+    return list_of_customers.length
+  end
+
 # UPDATE
 
 def update()
